e609bbb8b3fd73b5768aaf92c075b467eb52db6f
Niccolo Fato <niccolofato13@gmail.com>
2021-09-02 14:43:32 +0200

    Changes

--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class VideoService {
 
-  private apiUrl = 'https://niccolo-fato.github.io/youtube-clone/db.json/videos';
+  private apiUrl = 'https://niccolo-fato.github.io/youtube-clone/db.json';
 
   constructor(private http:HttpClient) { }
 
