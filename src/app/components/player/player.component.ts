64d1953e1d9bbf28604a728fdf6e3853da9858d3
Niccolo Fato <niccolofato13@gmail.com>
2021-09-05 21:26:41 +0200

    Changes to the video player

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -3,19 +3,21 @@ import { ActivatedRoute, Router } from "@angular/router";
 import { Component, OnInit } from "@angular/core";
 import { video } from "src/app/video";
 
-
 @Component({
   selector: "app-player",
   templateUrl: "./player.component.html",
   styleUrls: ["./player.component.css"],
 })
 export class PlayerComponent implements OnInit {
-
   id!: number;
   videos: video[] = [];
   mySubscription: any;
 
-  constructor( public activatedRoute: ActivatedRoute, private VideoService: VideoService, public router: Router ) {
+  constructor(
+    public activatedRoute: ActivatedRoute,
+    private VideoService: VideoService,
+    public router: Router
+  ) {
     this.VideoService.getvideos().subscribe((videos) => (this.videos = videos));
   }
 
@@ -27,5 +29,4 @@ export class PlayerComponent implements OnInit {
   getVideo() {
     this.id = Number(this.activatedRoute.snapshot.paramMap.get("id"));
   }
-  
 }
